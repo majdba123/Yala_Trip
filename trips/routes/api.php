d20246cb7f2912bf1c57758a5df7d55547fe7090
majdba123 <137396563+majdba123@users.noreply.github.com>
2024-05-12 15:11:25 +0300

    userapicontroller
    create auth register/login/logout n user api controller

--- trips/routes/api.php
+++ trips/routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserApiController;
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register',[UserApiController::class,'register']);
+Route::post('login',[UserApiController::class,'login']);
+Route::post('logout',[UserApiController::class,'logout'])->middleware('auth:sanctum');
